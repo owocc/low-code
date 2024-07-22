c21d71cc41e805809e293a6ef4a96b75c291f95a
owocc <owocc@qq.com>
2024-07-22 10:31:23 +0800

    fix: 修复样式bug

--- apps/designer/tailwind.config.js
+++ apps/designer/tailwind.config.js
@@ -3,7 +3,7 @@ const { iconsPlugin, getIconCollections } = require("@egoist/tailwindcss-icons")
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
